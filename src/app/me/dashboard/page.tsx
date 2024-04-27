012821f008f9ea32e8cbec2f4629d26f8d44eb05
Felipe Gomes <felipe.vgomes10.dev@gmail.com>
2024-04-27 11:39:59 -0300

    feat: create protected routes logic

--- src/app/me/dashboard/page.tsx
+++ src/app/me/dashboard/page.tsx
@@ -1,10 +1,7 @@
 import { auth } from "@/auth/auth";
-import { redirect } from "next/navigation";
 
 export default async function Dashboard() {
   const session = await auth();
 
-  if (!session) return redirect("/login");
-
-  return null;
+  return <pre>{JSON.stringify(session, null, 2)}</pre>;
 }
--- src/auth/auth.ts
+++ src/auth/auth.ts
@@ -1,4 +1,4 @@
-import NextAuth, { type DefaultSession } from "next-auth";
+import NextAuth, { type NextAuthConfig, type DefaultSession } from "next-auth";
 import GitHub from "next-auth/providers/github";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { prisma } from "../../prisma/client";
@@ -6,6 +6,7 @@ import { prisma } from "../../prisma/client";
 declare module "next-auth" {
   interface Session {
     user: {
+      id: string;
       name: string;
       email: string;
       image: string;
@@ -13,7 +14,29 @@ declare module "next-auth" {
   }
 }
 
-export const { signIn, signOut, auth, handlers } = NextAuth({
+export const authConfig = {
   providers: [GitHub],
   adapter: PrismaAdapter(prisma),
-});
+  callbacks: {
+    async session({ session, user }) {
+      session.user.id = user.id;
+      return session;
+    },
+    authorized({ auth, request: { nextUrl } }) {
+      const isLoggedIn = !!auth?.user;
+      const paths = ["/me"];
+      const isProtected = paths.some((path) =>
+        nextUrl.pathname.startsWith(path),
+      );
+
+      if (!isLoggedIn && isProtected) {
+        const redirectUrl = new URL("/login", nextUrl.origin);
+        return Response.redirect(redirectUrl);
+      }
+
+      return true;
+    },
+  },
+} satisfies NextAuthConfig;
+
+export const { signIn, signOut, auth, handlers } = NextAuth(authConfig);
