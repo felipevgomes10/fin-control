19d7b3f0914b8231889d85e3c8a2fcd219729c1f
Felipe Gomes <felipe.vgomes10.dev@gmail.com>
2024-04-29 19:41:50 -0300

    feat: show total amount

--- src/app/me/dashboard/fixed-expenses/page.tsx
+++ src/app/me/dashboard/fixed-expenses/page.tsx
@@ -1,5 +1,12 @@
 import { getFixedExpenses } from "@/actions/getFixedExpenses";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+} from "@/components/ui/card";
 import { DataTable } from "../../components/table/table";
+import { formatCurrency } from "../../components/table/utils";
 import { FixedExpensesDialog } from "./components/fixed-expenses-dialog";
 import { fixedExpensesColumns } from "./table-config/fixed-expenses-columns";
 
@@ -11,18 +18,33 @@ export default async function FixedExpenses() {
     amount,
     createdAt: createdAt.toUTCString(),
   }));
+  const totalAmount = data.reduce((acc, { amount }) => acc + amount, 0);
 
   return (
-    <section>
-      <DataTable
-        columns={fixedExpensesColumns}
-        data={data}
-        actions={
-          <div className="flex justify-end w-full mr-4">
-            <FixedExpensesDialog />
-          </div>
-        }
-      />
-    </section>
+    <>
+      <section>
+        <DataTable
+          columns={fixedExpensesColumns}
+          data={data}
+          actions={
+            <div className="flex justify-end w-full mr-4">
+              <FixedExpensesDialog />
+            </div>
+          }
+        />
+      </section>
+      <section>
+        <Card className="flex justify-end items-end flex-col">
+          <CardHeader>
+            <CardDescription>Total Fixed Expenses</CardDescription>
+          </CardHeader>
+          <CardContent>
+            <span className="text-2xl font-bold">
+              {formatCurrency(totalAmount)}
+            </span>
+          </CardContent>
+        </Card>
+      </section>
+    </>
   );
 }
