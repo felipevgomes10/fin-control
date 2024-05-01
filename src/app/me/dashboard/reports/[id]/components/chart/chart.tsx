6fa9c0fa67cc6e0ff48644dba0778aa386801f56
Felipe Gomes <felipe.vgomes10.dev@gmail.com>
2024-05-01 20:37:35 -0300

    fix: adjust chart labels

--- src/app/me/dashboard/reports/[id]/components/chart/chart.tsx
+++ src/app/me/dashboard/reports/[id]/components/chart/chart.tsx
@@ -18,29 +18,40 @@ export function Chart({
 
   return (
     <div className="flex justify-center h-[500px]">
-      <VictoryPie
-        theme={VictoryTheme.material}
-        range={[0, 100]}
-        categories={["Budget", "Total Expenses"]}
-        animate={{ duration: 2000 }}
-        colorScale={["#FFC107", "#FF5722"]}
-        style={{
-          labels: {
-            fill: theme === "dark" ? "white" : "black",
-            fontWeight: "bold",
-          },
-        }}
-        data={[
-          {
-            x: "Budget",
-            y: monthlyTargetExpensePercent - totalExpensesPercent,
-          },
-          {
-            x: "Total Expenses",
-            y: monthlyTargetExpensePercent,
-          },
-        ]}
-      />
+      <div className="grid grid-cols-[1fr_max-content] items-center">
+        <VictoryPie
+          theme={VictoryTheme.material}
+          range={[0, 100]}
+          categories={["Budget", "Total Expenses"]}
+          animate={{ duration: 2000 }}
+          colorScale={["#FFC107", "#FF5722"]}
+          style={{
+            labels: {
+              display: "none",
+            },
+          }}
+          data={[
+            {
+              x: "Budget",
+              y: monthlyTargetExpensePercent - totalExpensesPercent,
+            },
+            {
+              x: "Total Expenses",
+              y: monthlyTargetExpensePercent,
+            },
+          ]}
+        />
+        <div>
+          <div className="flex items-center gap-2">
+            <span className="w-3 h-3 rounded-full bg-[#FFC107]" />
+            <span className="w-[max-content]">Budget</span>
+          </div>
+          <div className="flex items-center gap-2">
+            <span className="w-3 h-3 rounded-full bg-[#FF5722]" />
+            <span className="w-[max-content]">Total Expenses</span>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
