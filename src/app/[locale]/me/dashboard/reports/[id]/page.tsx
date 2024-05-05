280f6c1b475345aaacec4ec4f6d21c5dc0586585
Felipe Gomes <felipe.vgomes10.dev@gmail.com>
2024-05-05 18:40:45 -0300

    feat: generate static params for reports

--- src/app/[locale]/me/dashboard/reports/[id]/page.tsx
+++ src/app/[locale]/me/dashboard/reports/[id]/page.tsx
@@ -1,7 +1,13 @@
 import { getExpensesReport } from "@/actions/reports/get-expenses-report";
+import { getExpensesReports } from "@/actions/reports/get-expenses-reports";
 import { getUserSettings } from "@/actions/user/get-user-settings";
 import { Suspense } from "react";
 import { ReportViewer } from "./components/report-viewer/report-viewer";
+
+export async function generateStaticParams() {
+  const reports = await getExpensesReports();
+  return reports.map((report) => ({ id: report.id }));
+}
 
 async function Content({ id }: { id: string }) {
   const [report, userSettings] = await Promise.all([
